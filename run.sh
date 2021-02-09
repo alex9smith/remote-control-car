8eb6abe1f1c6bf371787f112a5d336e5627d8d42
Alex Smith <alex@alexsmith.dev>
2021-02-09 16:34:48 +0000

    Make server listen on all interfaces

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 source venv/bin/activate
 pip install -r requirements.txt
-FLASK_APP="app" FLASK_ENV="development" flask run -h localhost -p "${PORT:-8081}"
+FLASK_APP="app" FLASK_ENV="development" flask run --host=0.0.0.0 -p "${PORT:-8081}"
